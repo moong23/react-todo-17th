af8e2a27b5286a5223d4d8ca29a238dd53cf87cc
moonki kim <moonki0623@naver.com>
2023-03-21 11:58:46 +0900

    [style]: css 초기화 [fix]: App.js 문법 최신화

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { RecoilRoot } from "recoil";
+import Mainpage from "./pages/Mainpage/Mainpage";
+
 function App() {
   return (
-    <div>
-      <h1>17기 프론트 화이팅~ 우하하</h1>
-    </div>
+    <RecoilRoot>
+      <Mainpage />
+    </RecoilRoot>
   );
 }
 
